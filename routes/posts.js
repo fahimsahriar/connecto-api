8f5854de1f9d1fb1efb5791ee899f12fcfc675d1
Fahim Sahriar <shishirluth@gmail.com>
2023-12-17 21:45:32 +0600

    final2

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,11 @@
 import Express from "express";
-import { getPosts, addPost, getblood } from "../controller/posts.js";
+import { getPosts, addPost, getblood, deletePost } from "../controller/posts.js";
 
 const router = Express.Router();
 
 router.get("/", getPosts);
 router.get("/blood", getblood);
 router.post("/add", addPost);
+router.delete("/:id", deletePost);
 
 export default router;
