f03e80e3c0ef2d97b0cc7ddfe6c558ca08bbf0d3
Fahim Sahriar <fahim.sahriar@sjinnovation.com>
2024-05-07 03:46:07 +0600

    changing infitifree files for deploying

--- connect.js
+++ connect.js
@@ -1,8 +1,8 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-  host:"localhost",
-  user:"root",
-  password:"",
-  database:"social"
+  host:"sql313.infinityfree.com",
+  user:"if0_36494747",
+  password:"3ny58Lcnsr",
+  database:"if0_36494747_connecto"
 })
